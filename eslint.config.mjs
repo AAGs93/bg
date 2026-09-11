05859a4d19259fe3d00c53cf3c92c19afa6d9217
Alessandro Agus <alessandro.agus93@gmail.com>
2026-09-11 10:11:25 +0200

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,24 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import nuxtPlugin from '@nuxt/eslint-plugin'
 
 export default withNuxt(
-  // Your custom configs here
+  // Tuo plugin Nuxt
+  nuxtPlugin,
+
+  // Le tue regole personalizzate qui
+  {
+    rules: {
+      // Esempio:
+      'vue/no-unused-components': 'off',
+      'vue/component-api-style': ['error', {
+        style: 'options',
+        allowedNames: ['setup']
+      }],
+      'vue/component-name-in-template-casing': ['error', 'PascalCase', {
+        registeredComponents: ['CategoryStories', 'FooterSection', 'HeroSection', 'MenuList', 'TemplateMenu'],
+        registeredVueComponents: ['CategoryStories', 'FooterSection', 'HeroSection', 'MenuList', 'TemplateMenu']
+      }]
+    }
+  }
 )
